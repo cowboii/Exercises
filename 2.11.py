5d4a51201c93b070e8c49ad1da8fa3633e415937
https://cowboii@github.com <cowboii@yandex.ru>
2019-09-09 16:19:29 +0200

    2.11 – done (wtf)

---
+++ 2.11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+a_list = ['Hello', 0]
+a_list.append(20.0)
+a_list.append('World')
+
+print(a_list)
+
+a_list = ['hello', 'I', 'love', 'Python', 'programming']
+a_list.remove('I')
+a_list.remove('love')
+
+print(a_list)
+
