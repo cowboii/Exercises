d7bd996c3ebf34020481dd8cc46f5564ace4d44b
https://cowboii@github.com <cowboii@yandex.ru>
2019-09-10 14:05:41 +0200

    add comments to 1.16

--- 1.16.py
+++ 1.16.py
@@ -3,12 +3,21 @@ from sys import argv
 
 
 def unit(int_num:int) -> int:
+    """
+    Return units of an int.
+    """
     return int(str(int_num)[-1])
 
 def ten(int_num:int) -> int:
+    """
+    Return tens of an int.
+    """
     return int(str(int_num)[-2])
 
 def swap_units_and_tens(int_num:int) -> int:
+    """
+    Swap units and tens in an int.
+    """
     return str(int_num)[:-2] + str(unit(int_num)) + str(ten(int_num))
 
 
